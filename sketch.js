02033b993ae0d4e6908a30af73d515644b196f97
skdog123 <86732132+skdog123@users.noreply.github.com>
2021-07-07 10:54:58 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var marks = [35,38,42,45,53,34,46,41,48,32];
+
+
+function setup(){
+  createCanvas(400,400);
+for(i=0;i<marks.length;i++){
+  if(marks[i]>45){
+console.log(marks[i])
+  }
+  }
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
